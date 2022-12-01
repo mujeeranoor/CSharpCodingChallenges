ad65f53758ac5c748b642fc3607531beda865954
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-01 20:47:57 +0500

    Convert Minutes into Seconds
    C# coding problem which converts minutes into seconds

---
+++ ConvertMinutesIntoSeconds/ConvertMinutesIntoSeconds/ConvertMINtoSEC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMinutesIntoSeconds
+{
+    class ConvertMINtoSEC
+    {
+        /// <summary>
+        /// Converts minutes into seconds
+        /// </summary>
+        /// <param name="minutes"></param>
+        /// <returns></returns>
+        public int convert(int minutes)
+        {
+            int m = minutes * 60;
+            Console.WriteLine(m);
+            return minutes;
+        }
+    }
+}
---
+++ ConvertMinutesIntoSeconds/ConvertMinutesIntoSeconds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMinutesIntoSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object of the class ConvertMINtoSEC
+            ConvertMINtoSEC c = new ConvertMINtoSEC();
+            c.convert(2); //calling method within the class
+            c.convert(3); //calling method within the class
+            Console.ReadLine();
+        }
+    }
+}
