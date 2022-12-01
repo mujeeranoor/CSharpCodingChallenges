c05a1029ccf03c6964701a100f561181b5917a55
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-01 22:09:16 +0500

    Return Next Number
    C# Program which returns the next number from the integer passed.

---
+++ ReturnNextNum/ReturnNextNum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnNextNum
+{
+    class Program
+    {
+        /// <summary>
+        /// Returns the next number from the integer passed.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int Addition(int num)
+        {
+            int n = num + 1;
+            Console.WriteLine(n);
+            return num;
+        }
+
+        /// <summary>
+        /// Main method of the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Creating object of the class Program
+            Program p = new Program();
+            p.Addition(33); //calling method
+            Console.ReadLine(); //for the console screen to stay
+        }
+    }
+}
