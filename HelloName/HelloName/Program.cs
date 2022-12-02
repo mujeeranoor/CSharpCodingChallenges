020ea5ef210277ebe7d591e9f65eb40ac8f6b5e1
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-02 21:17:17 +0500

    Hello Name
    C# program that returns name(input) with a greeting.

---
+++ HelloName/HelloName/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object of class program
+            Program p = new Program();
+            p.Greeting("Alexa"); // calling method
+            p.Greeting("Sam"); // calling method
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Returns "Hello (input name)"
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <returns>name with greeting</returns>
+        public string Greeting(string name)
+        {
+            Console.WriteLine("Hello " + name);
+            return name;
+        }
+    }
+}
