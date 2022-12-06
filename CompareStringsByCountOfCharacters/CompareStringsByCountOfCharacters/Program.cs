401d13dedd81031c83984f7e9a11d65acba04105
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-06 18:50:53 +0500

    Compare Strings by count of characters
    A C# coding challenge that checks if the strings are equal in length or not. Returns true or false according to it.

---
+++ CompareStringsByCountOfCharacters/CompareStringsByCountOfCharacters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStringsByCountOfCharacters
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //creating an object of class Program
+            p.Compare("Mujeera", "Noor"); // calling method
+        }
+
+        /// <summary>
+        /// returns true if strings are equal else returns false
+        /// </summary>
+        /// <param name="str1">string1</param>
+        /// <param name="str2">string2</param>
+        /// <returns>true or false</returns>
+        public bool Compare(string str1, string str2)
+        {
+            if (str1.Length == str2.Length)
+            {
+                Console.WriteLine("Strings are equal.");
+                Console.ReadLine();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Strings are not equal.");
+                Console.ReadLine();
+                return false;
+            }
+        }
+    }
+}
