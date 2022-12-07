c2f2632ffbc01787a37fc11b7458ef27c58ad36a
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-07 19:49:09 +0500

    Nth Even Number
    A C# coding challenge which takes integer input as index and gives output of even number of that index.

---
+++ NthEven/NthEven/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthEven
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //creates object for the class Program
+            p.NthEvenNumber(7); // calling method
+        }
+
+        /// <summary>
+        /// returns nth even number
+        /// </summary>
+        /// <param name="n">n</param>
+        /// <returns>nth even number</returns>
+        public int NthEvenNumber(int n)
+        {
+            int output = n * 2;
+            Console.WriteLine(output);
+            Console.ReadLine();
+            return n;
+        }
+    }
+}
