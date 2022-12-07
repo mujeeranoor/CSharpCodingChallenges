30c1d10f98c1a2fa4b19b9ba0f375b09efaee607
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-07 17:34:31 +0500

    Is the string even or odd
    C# coding challenge which takes a string as input and checks if its length is even or odd.

---
+++ StringOddOrEven/StringOddOrEven/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOddOrEven
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //creating object of class Program
+            p.OddOrEven("Mujeera"); //calling method
+        }
+
+        /// <summary>
+        /// Checks if the string length is even or odd.
+        /// </summary>
+        /// <param name="word">word</param>
+        /// <returns>true or false</returns>
+        public bool OddOrEven(string word)
+        {
+            int length = word.Length;
+            if (length % 2 == 0)
+            {
+                Console.WriteLine("String length is EVEN");
+                Console.ReadLine();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("String length is ODD");
+                Console.ReadLine();
+                return false;
+            }
+        }
+    }
+}
