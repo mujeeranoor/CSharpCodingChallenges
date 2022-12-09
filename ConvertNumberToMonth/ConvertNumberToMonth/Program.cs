20d0579f2079cf04863b9294477168e5c29f1953
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-09 16:40:51 +0500

    Convert Int month to string month
    A C# coding challenge which takes int month as input and returns string month as output. 
    (difficulty level: easy)

---
+++ ConvertNumberToMonth/ConvertNumberToMonth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertNumberToMonth
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program p = new Program(); // creating an object for the class Program
+            p.MonthName(7); // calling method
+        }
+
+        /// <summary>
+        /// Converts int month to string month
+        /// </summary>
+        /// <param name="month">month</param>
+        /// <returns>string month</returns>
+        public string MonthName(int month)
+        {
+            DateTime date = new DateTime(2022, month, 1);
+            Console.WriteLine(date.ToString("MMMM"));
+            Console.ReadLine();
+            return date.ToString("MMMM");
+            
+        }
+    }
+}
