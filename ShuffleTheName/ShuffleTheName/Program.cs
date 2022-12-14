6dde21275709cb73d8344c0beef8fa51bc18afe4
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2022-12-14 17:50:37 +0500

    Shuffle String
    A c# coding challenge which takes string as input and returns shuffled string.

---
+++ ShuffleTheName/ShuffleTheName/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleTheName
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //creating object for class Program
+            p.NameShuffle("Mujeera Noor"); // calling method
+        }
+
+        /// <summary>
+        /// Takes string input and prints shuffled string.
+        /// </summary>
+        /// <param name="str1">str1</param>
+        /// <returns>shuffled string</returns>
+        public string NameShuffle(string str1)
+        {
+            string[] result = str1.Split(' ');
+            Console.WriteLine(result[1] + " " + result[0]);
+            Console.ReadLine();
+            return "";
+        }
+    }
+}
