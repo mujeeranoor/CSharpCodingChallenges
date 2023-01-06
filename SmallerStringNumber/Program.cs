171b06b4b172e3982aecaabd306dbdb16bc41bcb
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-06 19:03:34 +0500

    adding another C# coding challenge

---
+++ SmallerStringNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SmallerStringNumber
+{
+    class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //creating object of a class Program
+            p.smallerNum("2","7"); //calling method
+        }
+
+        /// <summary>
+        /// Returns smallest string number
+        /// </summary>
+        /// <param name="n1">n1</param>
+        /// <param name="n2">n2</param>
+        /// <returns>Smallest string number</returns>
+        public string smallerNum(string n1, string n2)
+        {
+            string Result = Int32.Parse(n1) > Int32.Parse(n2) ? n2 : n1;
+            Console.WriteLine(Result);
+            Console.ReadLine();
+            return Result;
+        }
+    }
+}
