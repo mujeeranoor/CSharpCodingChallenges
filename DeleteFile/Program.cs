45bb617c0cbb549cf4d7a27c4618c9bc4350b65f
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-10 17:17:58 +0500

    Deletes a text file
    a C# coding challenge which finds text file from the given path and deletes it.

---
+++ DeleteFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static readonly string filepath = @"D:\Mujeera Noor\CSharp Workspace";
+        
+        static void Main(string[] args)
+        {
+            string mainFile = "hello.txt";
+
+            try
+            {
+                if (File.Exists(Path.Combine(filepath, mainFile)))
+                {
+                    File.Delete(Path.Combine(filepath, mainFile));
+                    Console.WriteLine("File has been deleted.");
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+            catch (IOException ioexp)
+            {
+                Console.WriteLine(ioexp.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
