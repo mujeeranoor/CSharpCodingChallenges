55319d82c6a9169f3a4162210f80a34950a743ae
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-10 22:56:48 +0500

    moves a file from one path to another

---
+++ MoveFile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace MoveFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"D:\Mujeera Noor\CSharp Workspace\people.txt";
+            string path2 = @"D:\Mujeera Noor\Terminal 123 Data\hello.txt";
+
+
+            try
+            {
+                //if (!File.Exists(path1))
+                //{
+                //    using (FileStream fs = File.Create(path1)) { }
+                //}
+
+                if (File.Exists(path2))
+                {
+                    Console.WriteLine("The file already exists");
+                }
+
+                if (File.Exists(path1))
+                {
+                    File.Move(path1, path2);
+                    Console.WriteLine("The file has been moved");
+                }
+
+                else
+                {
+                    Console.WriteLine("Code has been messed up");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed", e.ToString());
+            }
+            }   
+    }
+}
