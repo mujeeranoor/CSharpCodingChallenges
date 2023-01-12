7510679dd7cacaaf2f787ee852dfc45c3c194e87
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-12 21:16:27 +0500

    Get file names on the path

---
+++ GetxFiles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace GetxFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] GetFileName = Directory.GetFiles(@"D:\Mujeera Noor\Pythonworkspace");
+            for (int i = 0; i < GetFileName.Length; i++)
+            {
+                string path = GetFileName[i];
+                Console.WriteLine(System.IO.Path.GetFileName(path));
+            }
+            Console.ReadLine();
+        }
+    }
+}
