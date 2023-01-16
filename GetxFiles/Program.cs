de44100a9c90564be3b8c67f7f2ef75ea56e1131
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-16 15:24:02 +0500

    updated folder

--- GetxFiles/Program.cs
+++ GetxFiles/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.IO;
 
-namespace GetxFiles
+namespace GetxFiles2
 {
     class Program
     {
         static void Main(string[] args)
         {
             string[] GetFileName = Directory.GetFiles(@"D:\Mujeera Noor\Pythonworkspace");
+            string[] GetAllFilesFromDir = Directory.GetFiles(@"C:\Users\mujeera.noor\source\repos", "*.*", SearchOption.AllDirectories);
+
+
             for (int i = 0; i < GetFileName.Length; i++)
             {
                 string path = GetFileName[i];
                 Console.WriteLine(System.IO.Path.GetFileName(path));
+            }
+            Console.WriteLine();
+
+            for (int j = 0; j < GetAllFilesFromDir.Length; j++)
+            {
+                string pathDir = GetAllFilesFromDir[j];
+                Console.WriteLine();
+                Console.WriteLine(System.IO.Path.GetFileName(pathDir));
             }
             Console.ReadLine();
         }
