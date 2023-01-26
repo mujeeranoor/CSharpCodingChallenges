46094332331847f9837ca0f7d434173b759c5b37
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-26 22:46:10 +0500

    todays tasks

---
+++ JSONtoObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace JSONtoObject
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\mujeera.noor\source\repos\JSONtoObject\weatherForecast.json";
+            string jsonString = File.ReadAllText(fileName);
+            WeatherForecast weatherforecast = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+
+            Console.WriteLine($"Date: {weatherforecast.Date}");
+            Console.WriteLine($"TemperatureCelsius: {weatherforecast.TemperatureCelsius}");
+            Console.WriteLine($"Summary: {weatherforecast.Summary}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObjectToJSON/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ObjectToJSON
+{
+    public class Department
+    {
+        public int DeptId  { get; set;}
+        public string DepartmentName { get; set; }
+    }
+
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Department dept = new Department() { DeptId = 101, DepartmentName = "IT" };
+            string strJSON = JsonSerializer.Serialize<Department>(dept);
+            Console.WriteLine(strJSON);
+        }
+    }
+}
