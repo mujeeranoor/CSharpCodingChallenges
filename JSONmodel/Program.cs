b062fb02b89b01ba5f3cec04abf745595d2f1eef
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-01-30 21:40:55 +0500

    friday's practice task

---
+++ JSONmodel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace JSONmodel
+{
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string DOB { get; set; } 
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileHandle = @"C:\Users\mujeera.noor\source\repos\info.json";
+            string jsonString = File.ReadAllText(fileHandle);
+            PersonInfo personINFO = JsonSerializer.Deserialize<PersonInfo>(jsonString);
+
+            Console.WriteLine($"Name: {personINFO.Name}");
+            Console.WriteLine($"Age: {personINFO.Age}");
+            Console.WriteLine($"DOB: {personINFO.DOB}");
+            Console.ReadLine();
+        }
+    }
+}
