53130255cf565946a84ce3831b71bbb4c26f3c39
Mujeera Noor Thebo <76741763+mujeeranoor@users.noreply.github.com>
2023-02-01 19:42:41 +0500

    adding another task file

---
+++ JsonArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+
+namespace JsonArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array1 = @"[{""Name"" : ""Samuel"", ""Age"" : ""29"", ""designation"" : ""Team Lead""},
+                                {""Name"" : ""Anna"", ""Age"" : ""33"", ""designation"" : ""Junior Developer(Team Member)""}]";
+
+            using JsonDocument doc = JsonDocument.Parse(array1);
+            JsonElement root = doc.RootElement;
+
+            Console.WriteLine(root);
+
+            var u1 = root[0];
+            var u2 = root[1];
+            Console.WriteLine(u1);
+            Console.WriteLine(u2);
+
+            Console.WriteLine(u1.GetProperty("Name"));
+            Console.WriteLine(u1.GetProperty("Age"));
+            Console.WriteLine(u1.GetProperty("designation"));
+
+            Console.WriteLine(u2.GetProperty("Name"));
+            Console.WriteLine(u2.GetProperty("Age"));
+            Console.WriteLine(u2.GetProperty("designation"));
+        }
+    }
+}
